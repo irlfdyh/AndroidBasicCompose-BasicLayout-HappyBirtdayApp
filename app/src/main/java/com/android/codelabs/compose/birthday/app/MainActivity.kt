093ecf437adf7959023da995b9d6600defbf3c41
Irshal Firdeansyah <irshalfir@gmail.com>
2023-10-22 13:17:47 +0700

    update text properties

--- app/src/main/java/com/android/codelabs/compose/birthday/app/MainActivity.kt
+++ app/src/main/java/com/android/codelabs/compose/birthday/app/MainActivity.kt
@@ -3,13 +3,20 @@ package com.android.codelabs.compose.birthday.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.android.codelabs.compose.birthday.app.ui.theme.HappyBirthdayAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(
+                        message = "Happy Birthday Delia!",
+                        from = "Your Lovely"
+                    )
                 }
             }
         }
@@ -30,17 +40,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End),
+            fontSize = 36.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HappyBirthdayAppTheme {
-        Greeting("Android")
+        Greeting(
+            message = "Happy Birthday Delia!",
+            from = "Your Lovely"
+        )
     }
 }
