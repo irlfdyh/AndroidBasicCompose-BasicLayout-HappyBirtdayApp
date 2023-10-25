5ae1d1f91efa89d7d225a78cf9aa0d6a3e0869a0
Irshal Firdeansyah <irshalfir@gmail.com>
2023-10-25 10:37:22 +0700

    add greeting image

--- app/src/main/java/com/android/codelabs/compose/birthday/app/MainActivity.kt
+++ app/src/main/java/com/android/codelabs/compose/birthday/app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.android.codelabs.compose.birthday.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(
+                    GreetingText(
                         message = "Happy Birthday Delia!",
                         from = "Your Lovely"
                     )
@@ -40,7 +42,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(message: String, from: String, modifier: Modifier = Modifier) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.Center
@@ -61,11 +63,17 @@ fun Greeting(message: String, from: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+private fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Image(painter = image, contentDescription = null)
+}
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BirthdayCardPreview() {
     HappyBirthdayAppTheme {
-        Greeting(
+        GreetingImage(
             message = "Happy Birthday Delia!",
             from = "Your Lovely"
         )
